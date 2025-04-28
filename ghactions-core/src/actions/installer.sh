dd84766da81b28b45455773a92b7ea2a3283a0c4
GeekMasher <geekmasher@gmail.com>
2025-04-28 19:24:32 +0100

    feat(core): Update installer script

--- ghactions-core/src/actions/installer.sh
+++ ghactions-core/src/actions/installer.sh
@@ -1,15 +1,16 @@
 # This script installs the specified version of the action from GitHub releases.
 set -e
 
-VERSION="${ACTION_REF:-latest}"
-if [ "$VERSION" = "latest" ] || [ "$VERSION" = "main" ]; then
+if [[ ! "$ACTION_REF" =~ ^v?[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
     VERSION=$(gh release list --repo "$ACTION_REPOSITORY" --limit 1 | awk '{print $1}')
+else
+    VERSION="v${VERSION}"
 fi
 echo "GitHub Action Repository :: $ACTION_REPOSITORY (${VERSION})"
 echo "Runner :: $RUNNER_OS ($RUNNER_ARCH)"
 
 gh release download \
-    "v${VERSION}" \
+    "${VERSION}" \
     --repo "https://github.com/$ACTION_REPOSITORY" \
     --pattern "${BINARY_NAME}-${RUNNER_OS}-${RUNNER_ARCH}.tar.gz" --clobber \
     --output "/tmp/${RUNNER_OS}-${RUNNER_ARCH}.tar.gz"
